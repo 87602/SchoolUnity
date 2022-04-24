238f3619c6f9c3d2a6e4df3cd69227b500412dd0
87602 <93334168+87602@users.noreply.github.com>
2022-04-24 19:49:26 +0200

    update fix game

--- SchoolProjectUnityGame/Assets/highScore.cs
+++ SchoolProjectUnityGame/Assets/highScore.cs
@@ -19,6 +19,34 @@ public class highScore : MonoBehaviour
         StartCoroutine(Upload());
     }
 
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            //display highscores from database
+            HighscoreTxt.text = "Highscores:\n" + webRequest.downloadHandler.text;
+
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError(pages[page] + ": Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError(pages[page] + ": HTTP Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                    break;
+            }
+        }
+    }
 
 
     IEnumerator Upload()
